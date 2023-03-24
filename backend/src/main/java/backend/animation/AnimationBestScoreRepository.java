0bd5c409485ddff15fe3ec2bea04eefa4f81e81b
SujeongJo <suz.dev33@gmail.com>
2023-03-24 16:03:53 +0900

    🐛 Fix: 애니메이션 전체 리스트 조회 repository 수정
    1. 삭제 쿼리문 재작성

--- backend/src/main/java/backend/animation/AnimationBestScoreRepository.java
+++ backend/src/main/java/backend/animation/AnimationBestScoreRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AnimationBestScoreRepository extends JpaRepository<AnimationBestScoreEntity, Long> {
+    @Query("select a.bestScore from AnimationBestScoreEntity a where a.animationId = :animationId and a.userId = :userId")
+    Long findBestScoreByAnimationIdAndUserId(Long animationId, String userId);
+
     AnimationBestScoreEntity findByAnimationIdAndUserId(Long animationId, String userId);
 }
