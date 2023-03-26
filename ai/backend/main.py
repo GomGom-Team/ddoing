ec5edebabe2d0cd99270943faf047e64e045611f
sungminlim <parody9c@gmail.com>
2023-03-27 00:49:59 +0900

    ✨ Feat:  model serving 아직 결과는 숫자로 반환하는 형태 추가로 작업 할 예정

--- ai/backend/main.py
+++ ai/backend/main.py
@@ -1,5 +1,7 @@
+import cv2
+import numpy as np
 import tensorflow as tf
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from tensorflow.keras.applications import EfficientNetV2B0
 
 app = FastAPI()
@@ -14,6 +16,18 @@ async def root():
 
 	return return_dict
 
+@app.post("/inference")
+async def inference(file : UploadFile):
+	encode_to_int = np.fromstring(file.file.read(), dtype=np.uint8)
+	img = cv2.imdecode(encode_to_int, cv2.IMREAD_COLOR)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	img = cv2.resize(img, (64, 64))
+	img = np.reshape(img, (64, 64, 1))
+	cv2.imwrite('test.PNG', img)
+	result = model.predict(np.reshape(img, (1, 64, 64, 1)))
+	result = np.argsort(-result, axis=1)[:, :3]
+	return dict(result=np.squeeze(result).tolist())
+
 
 @app.on_event("startup")
 async def init_model():
@@ -21,9 +35,6 @@ async def init_model():
 	model_path = 'efficientnetv2-b0_model.h5'
 	shape = (64, 64, 1)
 	num_classes = 340
-	#model_path = 'imagenet'
-	#shape = (224, 224, 3)
-	#num_classes = 1000
 	print('load model start')
 	with tf.device('/cpu:0'):
 		model = EfficientNetV2B0(input_shape = shape, weights=model_path, classes=num_classes)
