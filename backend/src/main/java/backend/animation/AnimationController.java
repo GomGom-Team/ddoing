934066daaf3e716ec73e0902c689fd99b3307bf3
SujeongJo <suz.dev33@gmail.com>
2023-03-29 13:39:47 +0900

    ✨ Feat: 메인 페이지 인기리스트 조회
    1. DB 추가 필요

--- backend/src/main/java/backend/animation/AnimationController.java
+++ backend/src/main/java/backend/animation/AnimationController.java
@@ -19,30 +19,30 @@ public class AnimationController {
 
     // 영상 전체 리스트
     @GetMapping("/{userId}")
-    public ResponseEntity selectAllAnimations(@PathVariable String userId){
-        if(animationService.getAnimations(userId).size() < 1){
-                return ResponseEntity.noContent().build();
-        }else{
+    public ResponseEntity selectAllAnimations(@PathVariable String userId) {
+        if (animationService.getAnimations(userId).size() < 1) {
+            return ResponseEntity.noContent().build();
+        } else {
             return ResponseEntity.ok(animationService.getAnimations(userId));
         }
     }
 
     // 영상 상세
     @GetMapping("/{id}/{userId}")
-    public ResponseEntity selectOneAnimation(@PathVariable Long id, @PathVariable String userId){
+    public ResponseEntity selectOneAnimation(@PathVariable Long id, @PathVariable String userId) {
         return ResponseEntity.ok(animationService.getAnimation(id, userId));
     }
-    
+
     // 스크립트 정보 불러오기
     @GetMapping("/script/{id}")
-    public ResponseEntity getScripts(@PathVariable Long id){
+    public ResponseEntity getScripts(@PathVariable Long id) {
         return ResponseEntity.ok(animationService.getScripts(id));
     }
 
     // 애니메이션 점수 갱신
     @PostMapping("/score")
-    public ResponseEntity insertAnimationScore(@RequestBody AnimationRequestDTO animationRequestDTO){
-        if(!animationService.createScore(animationRequestDTO)){
+    public ResponseEntity insertAnimationScore(@RequestBody AnimationRequestDTO animationRequestDTO) {
+        if (!animationService.createScore(animationRequestDTO)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Message("점수 저장 실패"));
         }
         return ResponseEntity.ok(animationService.getUserScores(animationRequestDTO));
@@ -60,7 +60,7 @@ public class AnimationController {
 
     // 영상 필터링
     @GetMapping("/filter/star/{userId}/{star}")
-    public ResponseEntity filterByScores1(@PathVariable String userId, @PathVariable int star){
+    public ResponseEntity filterByScores1(@PathVariable String userId, @PathVariable int star) {
         if (animationService.filterAnimationsByScores(userId, star).size() < 1) {
             return ResponseEntity.noContent().build();
         } else {
@@ -70,7 +70,7 @@ public class AnimationController {
 
     // 영상 필터링 (수강유무)
     @GetMapping("/filter/{userId}/{done}")
-    public ResponseEntity filterByAlreadyDone(@PathVariable String userId, @PathVariable int done){
+    public ResponseEntity filterByAlreadyDone(@PathVariable String userId, @PathVariable int done) {
         if (animationService.filterAnimationsAlreadyDone(userId, done).size() < 1) {
             return ResponseEntity.noContent().build();
         } else {
@@ -80,8 +80,18 @@ public class AnimationController {
 
     // 발음 평가
     @PostMapping("/evaluate")
-    public ResponseEntity PronunciationEvaluation(@RequestParam String script, @RequestPart MultipartFile multipartFile){
-        int result =  animationService.evaluateScript(script, multipartFile);
+    public ResponseEntity PronunciationEvaluation(@RequestParam String script, @RequestPart MultipartFile multipartFile) {
+        int result = animationService.evaluateScript(script, multipartFile);
         return ResponseEntity.ok(result);
+    }
+
+    // 영상 인기리스트
+    @GetMapping("/top6/{userId}")
+    public ResponseEntity getAnimationsTop6(@PathVariable String userId) {
+        if (animationService.getAnimationsTop6List(userId).size() < 1) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(animationService.getAnimationsTop6List(userId));
+        }
     }
 }
