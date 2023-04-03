3c8b220c25db526c5b587d6b33e7604982cf6aee
yunji9151 <yunji9151@naver.com>
2023-04-03 14:38:48 +0900

    ✨ Feat: Top6 로직 수정
    1. Top6 상위 페이지에서 불러오는 것으로 수정

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,43 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import tw, { css, styled, theme } from 'twin.macro'
-import { NewContent, Animation, Drawing, HallofFame, Banner, PopularContents } from '../components/carousel/index'
-import { Container, Header, Footer } from '../components/common/index'
+import React, { useState, useEffect } from "react";
+import tw, { css, styled, theme } from "twin.macro";
+import {
+  NewContent,
+  Animation,
+  Drawing,
+  HallofFame,
+  Banner,
+  PopularContents,
+} from "../components/carousel/index";
+import { Container, Header, Footer } from "../components/common/index";
+import { useAppDispatch, useAppSelector } from "../redux/configStore.hooks";
+import { animationTop6GetAction } from "../redux/modules/animation";
 
-
-
-const items = [
-  <NewContent/>,
-  <Animation/>,
-  <Drawing/>,
-];
-
+const items = [<NewContent />, <Animation />, <Drawing />];
 
 const MainPage = () => {
+  const dispatch = useAppDispatch();
+  const topVideoList = useAppSelector(
+    (state) => state.animation.getAnimationTop6
+  );
+
+  useEffect(() => {
+    dispatch(animationTop6GetAction());
+  }, []);
 
   return (
     <Container isOverflowed>
-    {/* Header */}
-      <Header/>
+      {/* Header */}
+      <Header />
       <DummyDiv></DummyDiv>
 
-    {/* Banner */}
+      {/* Banner */}
       <Banner></Banner>
-    {/* // Popular Contents */}
-      <PopularContents></PopularContents>
+      {/* // Popular Contents */}
+      <PopularContents videoList={topVideoList}></PopularContents>
 
-    {/* 명예의 전당 */}
+      {/* 명예의 전당 */}
       <HallofFame></HallofFame>
-    {/* <HallofFame></HallofFame> */}
+      {/* <HallofFame></HallofFame> */}
 
-    {/* Footer  */}
-      <Footer/>
+      {/* Footer  */}
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
 // style
-const DummyDiv = styled.div(
-  tw`h-16`
-)
+const DummyDiv = styled.div(tw`h-16`);
 
-export default MainPage
+export default MainPage;
