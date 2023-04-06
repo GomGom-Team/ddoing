7c69fe2c84e257bb82394f1263f11e694b4b3a2a
parody9c@gmail.com <parody9c@gmail.com>
2023-04-06 12:48:13 +0000

    ♻️  Refactor: model serving code
    1. bash script 추가 (run.sh) chmod +x 필요
    2. category pickle 추가

--- ai/backend/new_main.py
+++ ai/backend/new_main.py
@@ -6,8 +6,7 @@ import tensorflow as tf
 from io import BytesIO
 from PIL import Image
 from typing_extensions import Annotated
-from fastapi import FastAPI, File, UploadFile, Body, Form
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, File, Body, Form
 from fastapi.middleware.cors import CORSMiddleware
 from tensorflow.keras.applications import EfficientNetV2B0
 
@@ -22,6 +21,8 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'])
 
+def normalize(image):
+	return cv2.normalize(src=image, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
 @app.get("/")
 async def root():
@@ -35,55 +36,29 @@ async def root():
 @app.post("/inference")
 async def inference(stage : Annotated[int, Form()] ,file : bytes = File()):
 	global categories
-	#file = str.encode(file)
 	print(file[:50], type(file))
 	file = base64.b64decode(file)
 
 	img = Image.open(BytesIO(file))
-	#print(img.shape)
 	img = np.array(img)[:,:,1:]
 	cv2.imwrite('test4.PNG', img)
-	print(img.shape)
+	#print(img.shape)
 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	img = normalize(img)
 	cv2.imwrite('test3.PNG', img)
-        #padding = 10
-        resize_ary = np.nonzero(img)
-	min_max = [(np.min(resize_ary[i]), np.max(resize_ary[i])) for i in range(len(resize_ary))]
-        min_x, max_x , min_y, max_y = min_max[0][0], min_max[0][1], min_max[1][0], min_max[1][1]
-        img = img[min_x:max_x, min_y:max_y]
-        img = cv2.resize(img, (64, 64))
+	img = cv2.resize(img, (64, 64))
 	cv2.imwrite('test2.PNG', img)
 	img = np.reshape(img, (64, 64, 1))
 	cv2.imwrite('test1.PNG', img)
-	result = model.predict(np.reshape(img, (1, 64, 64, 1)), batch_size=1)
+	result = model.predict(np.reshape(img, (1, 64, 64, 1)))
 	labels = np.argsort(-result, axis=1)[:, :3]
-	return_dict = dict(stage=stage, results=dict())
+	return_dict = dict(stage=stage, image='helloworld', results=dict())
 	for label in np.squeeze(labels).tolist():
-		print(label, result[:, label])
+		#print(label, result[:, label])
 		return_dict['results'][categories[label]] =  result[:, label].item()
 		#return_dict['results'][label] =  result[:, label].item()
 	print(return_dict)
 	return return_dict
-
-
-@app.post('/test')
-async def test_logic(stage : int, file : bytes = File(...)):
-	
-	#contents = await file.read()
-	contents =base64.b64decode(file)
-	pil_img = Image.open(BytesIO(contents))
-	np_img = np.array(pil_img)
-
-	result_dict = dict(state=stage, results=dict())
-	result_dict['results']['test1'] = 0.5
-	result_dict['results']['test2'] = 0.3
-	result_dict['results']['test3'] = 0.2
-	
-	metadata = {"filename" : file.filename, "shape" : np_image.shape}
-	headers= {"Content-Disposition" : "attachment", "filename": "image.png"}
-	
-	return FileResponse(ByteIO(contents), media_type='image/png', headers=headers, metadata=metadata, **result_dict)
-
 
 @app.on_event("startup")
 async def init_model():
@@ -93,8 +68,8 @@ async def init_model():
 	shape = (64, 64, 1)
 	num_classes = 340
 
-        with open('class_pickle_db', 'rb') as f:
-          categories = pickle.load(f)
+	with open('class_pickle_db', 'rb') as f:
+		categories = pickle.load(f)
 
 	print('load model start')
 	model = EfficientNetV2B0(input_shape = shape, weights=model_path, classes=num_classes)
